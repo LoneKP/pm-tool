3debe18e52565473555965e952a121c0106decdf
Lone KP <lonekp@Lones-MacBook.local>
2018-02-08 22:43:14 +0100

    setup projects controller and view html

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,10 @@
+class ProjectsController < ApplicationController
+	
+	def home
+	end
+	
+	def about
+	end
+	
+	
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+	root 'projects#home'
+	
+	get 'about', to: 'projects#about'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
