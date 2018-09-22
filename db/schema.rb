3f6d426db1825adf2f59200deae229218fc3788e
Lone Kjeldgaard <lone.kjeldgaard.pedersen@gmail.com>
2018-09-22 13:20:24 +0200

    created users table and user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180920060010) do
+ActiveRecord::Schema.define(version: 20180922111018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,13 @@ ActiveRecord::Schema.define(version: 20180920060010) do
     t.float "total_hours_meetings"
     t.float "total_hours_daily_standup"
     t.index ["project_id"], name: "index_time_trackings_on_project_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
