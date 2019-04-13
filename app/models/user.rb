73400905193e9f4ee183660b60c2b07733a0d861
Lone Kjeldgaard <lone.kjeldgaard.pedersen@gmail.com>
2019-04-13 23:41:07 +0200

    Validation error styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,9 @@ class User < ApplicationRecord
   validates :last_name, length: { minimum: 2 }
   validates :last_name, length: { maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, uniqueness: { case_sensitive: false, message: 'Hey! We know this email already, so it seems that we have met before. Please go to the <a href="%{link}">log in page</a> to log in with this email.', :link => Rails.application.routes.url_helpers.root_path  }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 
   belongs_to :organisation, inverse_of: :users, optional: true
   validates_presence_of :organisation
+
 end
